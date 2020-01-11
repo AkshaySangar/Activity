c6c11468203416c3cbe0ec81a2b700ba87342116
AkshaySangar <59762080+AkshaySangar@users.noreply.github.com>
2020-01-11 16:06:39 +0530

    Create Activity1.c

---
+++ Activity1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int num1 = 10;
+  int num2 = 5;
+  int res;
+  
+  res=num1+num2;
+  
+  if(res%2==0)
+    printf("Result is even");
+  else
+    printf("Result is odd");
+  
+}
