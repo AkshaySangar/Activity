79f881bd859b08119581c08f70d8cde1dceb4374
AkshaySangar <59762080+AkshaySangar@users.noreply.github.com>
2020-01-11 16:22:38 +0530

    Update Activity1.c

--- Activity1.c
+++ Activity1.c
@@ -5,12 +5,11 @@ int main()
   int num2 = 5;
   int res;
   
-  res=num1+num2;
+  res=num1+num2;   
   printf("Result of addition is : %d\n",res);
   
   if(res%2==0)
     printf("Result is even");
   else
     printf("Result is odd");
-  
 }
