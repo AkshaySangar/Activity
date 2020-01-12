55007f9190eec2bbed9fb29e7776c0fd5d9dd2a0
AkshaySangar <59762080+AkshaySangar@users.noreply.github.com>
2020-01-12 20:33:48 +0530

    Update Activity1.c

--- Activity1.c
+++ Activity1.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main()
 {
-  int num1 = 10;
-  int num2 = 5;
+  int num1;
+  int num2;
   int res,rem;
   
+  scanf("%d %d",&num1,&num2);
   res=num1+num2;   
   printf("Result of addition is : %d\n",res);
   rem=res%2;
